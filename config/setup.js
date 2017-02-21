16b0f6fe3d9a05b785cd1a06d6d31cda8676513f
Vitor Sousa <vitorsousa@Vitors-Air.isc.br.ibm.com>
2017-02-21 16:33:40 -0300

    env => UK (servico de US bugado)

--- config/setup.js
+++ config/setup.js
@@ -2,12 +2,12 @@
 /* Copyright 2016 IBM Corp. All Rights Reserved.
  * blockchain instance setup using ibc-js sdk
  * first implementation by Vitor Diego
-*/
+ */
 //------------------------------------------------------------------------------
 'use strict'
 
 //loads environment variables for blockchain setup (USA server - blockchain-go)
-const env = require('../env/env.json'); //env.json(for usa servers)
+const env = require('../env/uk_env.json'); //env.json(for usa servers)
 //const env = require('../env/local_env.json');
 const api = require('../rest/api.js');
 console.log(`getting environment variables \n ${env.peers[0].api_port_tls}`);
@@ -20,7 +20,10 @@ function runNetwork() {
     //index of users[] to be registered
     let x = 4;
     let rest = api(peers[0].api_host, peers[0].api_port_tls);
-    let admin = { user: env.users[x].enrollId, secret: env.users[x].enrollSecret };
+    let admin = {
+        user: env.users[x].enrollId,
+        secret: env.users[x].enrollSecret
+    };
 
     let getRegistrar = function (req) {
         rest.invoke.registrar(req.user, req.secret);
